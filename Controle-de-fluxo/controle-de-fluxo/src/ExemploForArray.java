6989beaf88fab9cc6b6b5e7d5183bc3b2dff60b0
Isaac764 <isaccruz764@gmail.com>
2024-07-19 12:16:38 -0300

    Estrutura de repetição

---
+++ Controle-de-fluxo/controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+
+        //em arrays o indicie de elementos se inicia em "0" ZERO
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+	   for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+       }
+    }  
+}
